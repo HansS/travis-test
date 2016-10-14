cd0657769e9797e6462646b7f8d1f9d8b1a966e2
hansschenker <gian.schenker@gmail.com>
2016-10-14 14:22:15 +0200

    initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var calc = require('../calculator');
+
+var expected = 5;
+var result = calc.add(2,3);
+
+assert( expected === expected, 'expected to be:' + expected);
+console.log(expected);
+
